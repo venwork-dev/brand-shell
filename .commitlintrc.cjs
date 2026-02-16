c3c583a5c4999d73ad16e08f2af47efc750a63fc
Venkatesh Bommadevara <121696481+venwork-dev@users.noreply.github.com>
2026-02-16 13:13:34 -0600

    feat(release): Create release workflows (#1)
    * feat(release): add release automation and commit policy checks
    
    * feat(release): changeset creation

---
+++ .commitlintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "perf", "refactor", "docs", "chore", "ci", "test", "style", "build", "revert"],
+    ],
+  },
+};
